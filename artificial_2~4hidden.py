58f610527d32a1c860f793d217be80adf5103349
Hao <sunhopht@gmail.com>
2017-06-08 21:02:30 +0800

    Create artificial_2~4hidden.py

---
+++ artificial_2~4hidden.py
@@ -0,0 +1,488 @@
+
+# coding: utf-8
+
+# In[337]:
+
+import numpy as np
+import os
+import pandas as pd
+
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+# In[498]:
+
+'''''This script demonstrates how to build a variational autoencoder with Keras. 
+ 
+Reference: "Auto-Encoding Variational Bayes" https://arxiv.org/abs/1312.6114 
+'''  
+import numpy as np  
+import matplotlib.pyplot as plt  
+from scipy.stats import norm  
+  
+from keras.layers import Input, Dense, Lambda,Conv2D, MaxPooling2D,Flatten, Reshape
+from keras.models import Model  
+from keras import backend as K  
+from keras import objectives  
+from keras.datasets import mnist  
+from keras.utils.vis_utils import plot_model  
+import sys  
+#import tensorflow as tf
+
+batch_size = 100  
+original_dim = 450 
+row = 15
+col = 15
+channel = 2
+latent_dim = 4
+intermediate_dim = 100
+nb_epoch = 100  
+epsilon_std = 1.0  
+
+#my tips:encoding  
+x = Input(shape=(original_dim,))
+#x_res = Reshape([row,col,channel])(x)
+#conv1 = Conv2D(32 ,(3,3),padding='same',kernel_initializer=keras.initializers.TruncatedNormal(mean=0.0, stddev=0.1, seed=0),activation='relu')(x_res)
+#pool1 = MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same', dim_ordering='default')(conv1)
+#conv2 = Conv2D(32 ,(3,3),padding='same',kernel_initializer=keras.initializers.TruncatedNormal(mean=0.0, stddev=0.1, seed=0),activation='relu')(pool1)
+#pool2 = MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='same', dim_ordering='default')(conv2)
+#conv_flat = Flatten()(pool1)
+#h = Dense(intermediate_dim, activation='relu')(conv_flat)  
+z_mean = Dense(latent_dim)(x)  
+z_log_var = Dense(latent_dim)(x)  
+
+#my tips:Gauss sampling,sample Z  
+def sampling(args):   
+    z_mean, z_log_var = args  
+    epsilon = K.random_normal(shape=(batch_size, latent_dim), mean=0.,stddev=epsilon_std)  
+    return z_mean + K.exp(z_log_var / 2) * epsilon  
+  
+# note that "output_shape" isn't necessary with the TensorFlow backend  
+# my tips:get sample z(encoded)  
+z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+
+
+# we instantiate these layers separately so as to reuse them later  
+#decoder_h = Dense(intermediate_dim, activation='relu')(z) 
+decoder_mean = Dense(original_dim, activation='sigmoid')(z)
+
+#x_res = np.reshape(x,(-1,450))
+
+def vae_loss(x, decoder_mean):  
+    xent_loss = original_dim * objectives.binary_crossentropy(x,decoder_mean)  
+    kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)  
+    return xent_loss + 0*kl_loss  
+  
+vae = Model(x, decoder_mean)  
+vae.compile(optimizer='rmsprop', loss=vae_loss)  
+
+newdata = pd.read_csv("tri-XYModel15_15simul.csv").values[17999:]
+T = pd.read_csv("tri-Temp15_15.csv").values[17999:]
+T = np.asarray(np.reshape(T,[-1,len(T)]))
+T = T[0]
+
+#for i in range(len(newdata)):
+#    for j in range(len(newdata[0])):
+#        newdata[i][j] = (newdata[i][j]+1)/2
+index = [i for i in range(len(newdata))]
+import random
+random.shuffle(index)
+newdata = newdata[index]
+T = T[index]
+x_train_1 =np.cos(newdata[0:2000]) 
+x_test_1 = np.cos(newdata[2000:2100])
+x_train_2 = np.sin(newdata[0:2000])
+x_test_2 = np.sin(newdata[2000:2100])
+#x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))  
+#x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))  
+
+
+
+# In[499]:
+
+x_train = np.hstack((x_train_1,x_train_2))
+x_test = np.hstack((x_test_1,x_test_2))
+#x_train = x_train.reshape((-1,row,col,channel))
+#x_test = x_test.reshape((-1,row,col,channel))
+
+
+# In[500]:
+
+import keras
+EarlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='auto')
+
+
+vae.fit(x_train, x_train,  
+        shuffle=True,  
+        nb_epoch=nb_epoch,  
+        #verbose=2,  
+        batch_size=100,  
+        validation_data=(x_test, x_test),callbacks=[EarlyStopping])  
+# encode and decode some digits
+# note that we take them from the *test* set
+# use Matplotlib (don't ask)
+
+
+# In[501]:
+
+import matplotlib.pyplot as plt
+
+#encoded_imgs = encoder.predict(x_test)
+decoded_imgs = vae.predict(x_test,batch_size=100)
+size = 15
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(1,n):
+    # display original
+    ax = plt.subplot(2,n,i)
+    plt.imshow(x_test[i][:225].reshape(size, size))
+    #plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n ,i + n)
+    plt.imshow(decoded_imgs[i][:225].reshape(size,size))
+    #plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
+
+# In[502]:
+
+
+y_test = np.zeros((len(x_test),))
+for i in range(len(x_test)):
+    y_test[i]= (np.sum(x_test[i])-512)*2
+
+T_test = T[2000:2100]
+
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space  
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)  
+plt.figure(figsize=(6, 6))  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 0] ,x_test_encoded[:, 1] , c=T_test)  
+plt.colorbar()
+plt.show()  
+  
+
+
+# In[507]:
+
+
+y_test = np.zeros((len(x_test),))
+for i in range(len(x_test)):
+    y_test[i]= (np.sum(x_test[i])-512)*2
+
+T_test = T[2000:2100]
+
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space  
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)  
+plt.figure(figsize=(6, 6))  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 0] ,x_test_encoded[:, 2] , c=T_test)  
+plt.colorbar()
+plt.show()  
+  
+
+
+# In[508]:
+
+
+y_test = np.zeros((len(x_test),))
+for i in range(len(x_test)):
+    y_test[i]= (np.sum(x_test[i])-512)*2
+
+T_test = T[2000:2100]
+
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space  
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)  
+plt.figure(figsize=(6, 6))  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 0] ,x_test_encoded[:, 3] , c=T_test)  
+plt.colorbar()
+plt.show()  
+  
+
+
+# In[509]:
+
+
+y_test = np.zeros((len(x_test),))
+for i in range(len(x_test)):
+    y_test[i]= (np.sum(x_test[i])-512)*2
+
+T_test = T[2000:2100]
+
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space  
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)  
+plt.figure(figsize=(6, 6))  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 1] ,x_test_encoded[:, 2] , c=T_test)  
+plt.colorbar()
+plt.show()  
+  
+
+
+# In[510]:
+
+
+y_test = np.zeros((len(x_test),))
+for i in range(len(x_test)):
+    y_test[i]= (np.sum(x_test[i])-512)*2
+
+T_test = T[2000:2100]
+
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space  
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)  
+plt.figure(figsize=(6, 6))  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 1] ,x_test_encoded[:, 3] , c=T_test)  
+plt.colorbar()
+plt.show()  
+  
+
+
+# In[511]:
+
+
+y_test = np.zeros((len(x_test),))
+for i in range(len(x_test)):
+    y_test[i]= (np.sum(x_test[i])-512)*2
+
+T_test = T[2000:2100]
+
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space  
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)  
+plt.figure(figsize=(6, 6))  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 2] ,x_test_encoded[:, 3] , c=T_test)  
+plt.colorbar()
+plt.show()  
+
+
+# In[390]:
+
+np.pi*120/180
+
+
+# In[389]:
+
+x_train[0]
+
+
+# In[417]:
+
+start = []
+for i in range(1000):
+    start.append(2*np.pi * np.random.random()-np.pi)
+
+
+# In[418]:
+
+start
+
+
+# In[465]:
+
+deg_120 = np.pi*120/180
+lattice = [] 
+for i in range(1000):
+    line_1 = []
+    line_2 = []
+    line_3 = []
+    for j in range(15):
+        line_1.append(start[i]+j*deg_120) 
+    for j in range(15):
+        line_2.append(start[i]+j*deg_120 + deg_120)
+    for j in range(15):
+        line_3.append(start[i]+j*deg_120 + 2 * deg_120)
+    lattice.append(np.reshape(np.vstack((line_1,line_2,line_3)*5),[225]))
+
+
+# In[467]:
+
+np.cos(lattice)
+
+
+# In[471]:
+
+np.cos(lattice[0])
+
+
+# In[451]:
+
+lattice = np.asarray(lattice)
+lattice.shape
+
+
+# In[456]:
+
+lat_cos = np.zeros((1000,225))
+lat_sin = np.zeros((1000,225))
+for i in range(1000):
+    lat_cos[i]= np.cos(lattice[i])
+    lat_sin[i]= np.sin(lattice[i])
+
+
+# In[457]:
+
+test_artificial = np.hstack((lat_cos,lat_sin))
+
+
+# In[459]:
+
+test_artificial.shape
+
+
+# In[512]:
+
+y_test = np.zeros((len(x_test),))
+for i in range(len(x_test)):
+    y_test[i]= (np.sum(x_test[i])-512)*2
+
+T_test = T[2000:2100]
+
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space  
+x_test_encoded = encoder.predict(test_artificial[:100], batch_size=batch_size)  
+plt.figure(figsize=(6, 6))  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 0] ,x_test_encoded[:, 1] , c=T_test)  
+plt.hold()
+x_test_encoded = encoder.predict(x_test[:100], batch_size=batch_size)  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 0] ,x_test_encoded[:, 1] , c=T_test)  
+plt.colorbar()
+plt.show()  
+  
+
+
+# In[472]:
+
+lat_cos[0]
+
+
+# In[482]:
+
+import matplotlib.pyplot as plt
+
+#encoded_imgs = encoder.predict(x_test)
+decoded_imgs = vae.predict(test_artificial,batch_size=100)
+size = 15
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(1,n):
+    # display original
+    ax = plt.subplot(2,n,i)
+    plt.imshow(test_artificial[i][:225].reshape(size, size))
+    #plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n ,i + n)
+    plt.imshow(decoded_imgs[i][:225].reshape(size,size))
+    #plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
+
+# In[483]:
+
+deg_120 = - np.pi*120/180
+lattice_2 = [] 
+for i in range(1000):
+    line_1 = []
+    line_2 = []
+    line_3 = []
+    for j in range(15):
+        line_1.append(start[i]+j*deg_120) 
+    for j in range(15):
+        line_2.append(start[i]+j*deg_120 + deg_120)
+    for j in range(15):
+        line_3.append(start[i]+j*deg_120 + 2 * deg_120)
+    lattice_2.append(np.reshape(np.vstack((line_1,line_2,line_3)*5),[225]))
+
+
+# In[486]:
+
+lat_cos_2 = np.zeros((1000,225))
+lat_sin_2 = np.zeros((1000,225))
+for i in range(1000):
+    lat_cos_2[i]= np.cos(lattice_2[i])
+    lat_sin_2[i]= np.sin(lattice_2[i])
+    
+test_artificial_2 = np.hstack((lat_cos_2,lat_sin_2))
+
+
+# In[513]:
+
+y_test = np.zeros((len(x_test),))
+for i in range(len(x_test)):
+    y_test[i]= (np.sum(x_test[i])-512)*2
+
+T_test = T[2000:2100]
+
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space  
+x_test_encoded = encoder.predict(test_artificial_2[:100], batch_size=batch_size)  
+plt.figure(figsize=(6, 6))  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 0] ,x_test_encoded[:, 1] , c=T_test)  
+plt.hold()
+x_test_encoded = encoder.predict(x_test[:100], batch_size=batch_size)  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 0] ,x_test_encoded[:, 1] , c=T_test)  
+plt.colorbar()
+plt.show()  
+  
+
+
+# In[524]:
+
+y_test = np.zeros((len(x_test),))
+for i in range(len(x_test)):
+    y_test[i]= (np.sum(x_test[i])-512)*2
+
+T_test = T[2000:2100]
+
+encoder = Model(x, z_mean)
+
+# display a 2D plot of the digit classes in the latent space  
+x_test_encoded = encoder.predict(test_artificial_2[:100], batch_size=batch_size)  
+plt.figure(figsize=(6, 6))  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 1] ,x_test_encoded[:, 0] , c=T_test)  
+plt.hold()
+x_test_encoded = encoder.predict(test_artificial[:100], batch_size=batch_size)  
+#plt.scatter( x_test_encoded[:, 0] ,y_test , c=T_test)  
+plt.scatter( x_test_encoded[:, 2] ,x_test_encoded[:, 1] , c=T_test)  
+plt.colorbar()
+plt.show()  
+  
+
+
+# In[ ]:
+
+
+
